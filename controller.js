272a1827f32d4106900089c5006eee1c62182e00
unknown <TaurusSHO89@gmail.com>
2018-06-12 19:12:35 -0600

    completed

---
+++ controller.js
@@ -0,0 +1,8 @@
+module.exports = {
+    getPlanes: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+        dbInstance.get_planes([25])
+            .then(planes => res.status(200).send(planes))
+            .catch(error => res.status(500).send(error))
+    }
+}
